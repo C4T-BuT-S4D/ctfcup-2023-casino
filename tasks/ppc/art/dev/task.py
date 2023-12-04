2e83d238db2c477458a12b2b1d6f9ed206a4579a
Ivan Fedotov <qumusabel@nomail>
2023-12-04 13:52:18 +0300

    reformat sources

--- tasks/ppc/art/dev/task.py
+++ tasks/ppc/art/dev/task.py
@@ -4,7 +4,6 @@ import random
 
 from rubik.cube import Cube
 
-random.seed(1337)  # FIXME
 
 IMAGE_W, IMAGE_H = 291, 15
 IMAGE_LEN = IMAGE_W * IMAGE_H
@@ -12,8 +11,9 @@ BG_COLORS = "BGORY"
 
 MOVES = "L Li R Ri U Ui D Di F Fi B Bi M Mi E Ei S Si X Xi Y Yi Z Zi".split()
 
+
 def nsplit(it, n):
-    return [it[i:i+n] for i in range(0, len(it), n)]
+    return [it[i : i + n] for i in range(0, len(it), n)]
 
 
 def load_text_mask() -> list[str | None]:
@@ -28,7 +28,7 @@ def generate_random_bg() -> str:
 
 def split_into_faces(linear_picture: str) -> list[tuple[int, int, str]]:
     lines = [nsplit(l, 3) for l in nsplit(linear_picture, IMAGE_W)]
-    
+
     faces = []
     for y, three_lines in enumerate(nsplit(lines, 3)):
         for x, what in enumerate(zip(*three_lines)):
@@ -59,18 +59,22 @@ def challenge_round(x: int, y: int, face: str):
         cube.sequence(moves)
 
         cube_str = str(cube)
-        cube_front_face = "\n".join((cube_str[28:28+3], cube_str[44:44+3], cube_str[60:60+3]))
+        cube_front_face = "\n".join(
+            (cube_str[28 : 28 + 3], cube_str[44 : 44 + 3], cube_str[60 : 60 + 3])
+        )
 
         if cube_front_face == face:
             break
-        
+
         print("Sorry, this doesn't look right. Let's try again.")
 
     print("Great! Moving on...")
 
 
 if __name__ == "__main__":
-    picture = "".join([fg or bg for fg, bg in zip(load_text_mask(), generate_random_bg())])
+    picture = "".join(
+        [fg or bg for fg, bg in zip(load_text_mask(), generate_random_bg())]
+    )
 
     print(f"Greetings! Let's cube a {IMAGE_W // 3}x{IMAGE_H // 3} picture!")
     print()
@@ -88,4 +92,3 @@ if __name__ == "__main__":
         challenge_round(x, y, face)
 
     print("...we're done. Thanks! Bye.")
-
