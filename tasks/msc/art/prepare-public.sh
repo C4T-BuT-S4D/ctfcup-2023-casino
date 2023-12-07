b580918a3ef75d5512fc45042d886c6a6e9651d3
Ivan Fedotov <qumusabel@nomail>
2023-12-07 21:53:02 +0300

    update deploy and public

--- tasks/msc/art/prepare-public.sh
+++ tasks/msc/art/prepare-public.sh
@@ -4,11 +4,11 @@ set -e
 set -x
 
 rm -rf public/
-mkdir -p public/art
+mkdir -p public/msc-art
 
-cp dev/fake-text.png public/art/text.png
-cp deploy/{Dockerfile,docker-compose.yml,task.py,requirements.txt} public/art/
+cp dev/fake-text.png public/msc-art/text.png
+cp deploy/{Dockerfile,docker-compose.yml,task.py,requirements.txt} public/msc-art/
 
 cd public/
 
-zip -r art.zip art/
+zip -r msc-art.zip msc-art/
