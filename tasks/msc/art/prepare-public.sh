9c8351ea505809128706e3f493854e854251e060
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2023-12-07 21:41:31 +0300

    art: remove extraneous nl from prepare-public, regen public art.zip

--- tasks/msc/art/prepare-public.sh
+++ tasks/msc/art/prepare-public.sh
@@ -4,12 +4,11 @@ set -e
 set -x
 
 rm -rf public/
-mkdir -p public/ppc-art
+mkdir -p public/art
 
-cp dev/fake-text.png public/ppc-art/text.png
-cp deploy/{Dockerfile,docker-compose.yml,task.py,requirements.txt} public/ppc-art/
+cp dev/fake-text.png public/art/text.png
+cp deploy/{Dockerfile,docker-compose.yml,task.py,requirements.txt} public/art/
 
 cd public/
 
-zip -r ppc-art.zip ppc-art/
-
+zip -r art.zip art/
