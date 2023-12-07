e8043c725fec3da8012f49e7faec29246f8c3a59
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2023-12-07 18:43:46 +0300

    enterprise-flagchecker: improve builder, add error handling to task

--- tasks/rev/enterprise-flagchecker/dev/build.sh
+++ tasks/rev/enterprise-flagchecker/dev/build.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t enterprise-flagchecker:latest .
-docker run --rm -it -v ./vol:/vol enterprise-flagchecker:latest cp /build/yatool/task/task /vol
+docker run --rm -it -v ./vol:/vol enterprise-flagchecker:latest cp /build/task /vol
