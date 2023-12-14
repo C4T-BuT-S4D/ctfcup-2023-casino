ac167389baa7e7c6ee1e8ae316cfc5de1873028c
slonser <sevakokorin80@gmail.com>
2023-12-14 23:47:24 +0300

    asd

---
+++ tasks/web/grekIlya/deploy/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const bot = require('./bot');
+const Recaptcha = require('express-recaptcha').RecaptchaV2;
+const recaptcha = new Recaptcha(process.env.CAPTCHA, process.env.CAPTCHA_SECRET);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.post('/bot', recaptcha.middleware.verify, (req, res) => {
+    if(!req.body || !req.body.url) 
+        return res.send('No url specified');
+    bot(req.body.url);
+    res.send('Admin will visit your url!');
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
+});
